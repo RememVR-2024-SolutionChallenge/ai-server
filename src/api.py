6d2c2272ea2362ae158a7d3f6986a7566cfb5f6c
seooyxx <byunhwan8832@korea.ac.kr>
2024-01-31 20:58:29 +0900

    feat(api): Extend api with ffmpeg

--- src/api.py
+++ src/api.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from starlette.responses import JSONResponse
 from typing import List, Optional
 
+from .ffmpeg.router import router as ffmpeg_router
 from .colmap.router import router as colmap_router
 
 class ErrorMessage(BaseModel):
@@ -10,9 +11,14 @@ class ErrorMessage(BaseModel):
 
 class ErrorResponse(BaseModel):
     detail: Optional[List[ErrorMessage]]
-
+    
 api_router = APIRouter(
     prefix="/engine"
+)
+
+
+api_router.include_router(
+    ffmpeg_router, prefix="/preprocess/ffmpeg", tags = ["ffmpeg"]
 )
 
 api_router.include_router(
