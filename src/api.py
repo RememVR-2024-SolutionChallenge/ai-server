8c60b5611988c5f769105ffe0480ad2db07b5af8
seooyxx <byunhwan8832@korea.ac.kr>
2024-01-31 21:13:41 +0900

    feat(api): Extend api with scene training

--- src/api.py
+++ src/api.py
@@ -5,6 +5,7 @@ from typing import List, Optional
 
 from .ffmpeg.router import router as ffmpeg_router
 from .colmap.router import router as colmap_router
+from .scene_model.router import router as scene_training_router
 
 class ErrorMessage(BaseModel):
     msg: str
@@ -16,11 +17,14 @@ api_router = APIRouter(
     prefix="/engine"
 )
 
-
 api_router.include_router(
     ffmpeg_router, prefix="/preprocess/ffmpeg", tags = ["ffmpeg"]
 )
 
 api_router.include_router(
     colmap_router, prefix="/preprocess/colmap", tags=["colmap"]
+)
+
+api_router.include_router(
+    scene_training_router, prefix="/train/scene", tags=["scene training"]
 )
